05423a409b76fa868918a86d7f63d54fc1f67ffe
rahibgasimli <rahib8425@gmail.com>
2025-08-03 13:57:20 +0400

    TRPC  configuration

---
+++ src/lib/ratelimit.ts
@@ -0,0 +1,8 @@
+import { Ratelimit } from "@upstash/ratelimit"
+
+import { redis } from "./redis"
+
+export const rateLimit = new Ratelimit({
+    redis,
+    limiter: Ratelimit.slidingWindow(5, "10s")
+})
